66761d30d8e146085c770daa7df56b1950bff3ac
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-11-23 16:24:40 +0300

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React, { useEffect } from "react";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import Main from "./components/Main";
-import CartPanel from "./components/CartPanel";
 import styled from "@emotion/styled";
 import handleScreenSize from "./settings/screenWidth";
 import { getItems } from "./store/manage";
 import { useDispatch } from "react-redux";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
 
 const AppStyled = styled.div({
     height: "100%",
 });
-
-const MyContainer = styled.div({
-    position: "absolute",
-    top: "50px",
-    bottom: "0px",
-    display: "flex",
-    width: "100%",
-});
-
-const Home = () => {
-    return (
-        <React.Fragment>
-            <MyContainer>
-                <Sidebar />
-                <Main />
-            </MyContainer>
-            <CartPanel />
-        </React.Fragment>
-    );
-};
 
 const Cart = () => {
     return <div>Fuck</div>;
---
+++ src/views/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Sidebar from "../components/Sidebar";
+import Main from "../components/Main";
+import CartPanel from "../components/CartPanel";
+
+const MyContainer = styled.div({
+    position: "absolute",
+    top: "50px",
+    bottom: "0px",
+    display: "flex",
+    width: "100%",
+});
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <MyContainer>
+                <Sidebar />
+                <Main />
+            </MyContainer>
+            <CartPanel />
+        </React.Fragment>
+    );
+};
+
+export default Home;
