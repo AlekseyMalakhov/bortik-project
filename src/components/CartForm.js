1e421dba328d7d59612d8c72b2b0d4ba58010686
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-11-24 18:07:34 +0300

    add formInput component

--- src/components/CartForm.js
+++ src/components/CartForm.js
@@ -4,14 +4,20 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import FormInput from "./FormInput";
 
 const CartFormStyled = styled.div({
     marginTop: "30px",
 });
 
 const validationSchema = Yup.object().shape({
-    fullname: Yup.string().required().label("Fullname"),
-    email: Yup.string().required().email().label("Email"),
+    name_user: Yup.mixed().required().label("Имя").typeError("Укажите имя"),
+    email: Yup.string().required().email().label("email").typeError("Укажите email"),
+    phone: Yup.string()
+        .matches(/\+\d{5}\d+/)
+        .required()
+        .label("Телефон")
+        .typeError("Укажите телефон"),
 });
 
 function CartForm() {
@@ -22,44 +28,24 @@ function CartForm() {
         <CartFormStyled>
             <Formik
                 initialValues={{
-                    fullname: "",
+                    name_user: "",
                     email: "",
+                    phone: "",
                 }}
                 validationSchema={validationSchema}
                 onSubmit={handleSubmit}
             >
                 {({ handleSubmit, handleChange, setFieldTouched, values, touched, errors }) => (
                     <Form noValidate onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="formBasicEmail111">
-                            <Form.Label>ФИО</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="fullname"
-                                value={values.fullname}
-                                onChange={handleChange}
-                                onBlur={() => setFieldTouched("fullname")}
-                                isValid={touched.fullname && !errors.fullname}
-                                isInvalid={touched.fullname ? errors.fullname : null}
-                            />
-                            <Form.Control.Feedback type="invalid">{errors.fullname}</Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicEmail11dfd1">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="email"
-                                value={values.email}
-                                onChange={handleChange}
-                                onBlur={() => setFieldTouched("email")}
-                                isValid={touched.email && !errors.email}
-                                isInvalid={touched.email ? errors.email : null}
-                            />
-                            <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
+                        <FormInput name="name_user" label="ФИО*" />
+                        <FormInput name="phone" label="Телефон*" inputMode="numeric" />
+                        <FormInput name="email" label="Email*" />
+                        {console.log(errors)}
+                        <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
+                            <Button variant="primary" type="submit">
+                                Отправить
+                            </Button>
+                        </div>
                     </Form>
                 )}
             </Formik>
---
+++ src/components/FormInput.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useFormikContext } from "formik";
+import Form from "react-bootstrap/Form";
+
+function FormInput({ name, label, ...otherProps }) {
+    const { setFieldTouched, setFieldValue, errors, touched, values } = useFormikContext();
+    return (
+        <Form.Group className="mb-3" controlId={"cart_form_" + [name]}>
+            <Form.Label>{label}</Form.Label>
+            <Form.Control
+                type="text"
+                name={name}
+                value={values[name]}
+                onChange={(e) => setFieldValue(name, e.target.value)}
+                onBlur={() => setFieldTouched(name)}
+                isValid={touched[name] && !errors[name]}
+                isInvalid={touched[name] ? errors[name] : null}
+            />
+            <Form.Control.Feedback type="invalid">{errors[name]}</Form.Control.Feedback>
+        </Form.Group>
+    );
+}
+
+export default FormInput;
