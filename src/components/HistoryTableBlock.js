01eb6c054e8e21de413afac9677579112461e828
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-06 11:22:40 +0300

    add history items to cart

--- src/components/HistoryTableBlock.js
+++ src/components/HistoryTableBlock.js
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 import Table from "react-bootstrap/Table";
 import HistoryTableRow from "./HistoryTableRow";
 import Button from "react-bootstrap/Button";
+import { useSelector, useDispatch } from "react-redux";
+import { addItemToCart } from "../store/manage";
 
 const HistoryTableBlockStyled = styled.div({
     marginBottom: "20px",
@@ -22,13 +24,60 @@ const createDate = (dateString) => {
     const year = date.getFullYear();
     const h = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     const m = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-
     const result = `${day}.${month}.${year} - ${h}:${m}`;
-
     return result;
 };
 
 function HistoryTableBlock({ order }) {
+    const dispatch = useDispatch();
+    const items = useSelector((state) => state.manage.items);
+
+    const putItemToCart = (orderItem) => {
+        //console.log(orderItem);
+
+        //for test only
+        const orderItemTest = { ...orderItem };
+        if (orderItemTest.article === "SMART.12026") {
+            orderItemTest.article = "test";
+        }
+        //end test
+
+        for (let x in items) {
+            const category = items[x];
+            if (category.length > 0) {
+                for (let i = 0; i < category.length; i++) {
+                    const item = category[i];
+                    if (item.article === orderItemTest.article) {
+                        const obj = {
+                            ...item,
+                            number: orderItemTest.number,
+                        };
+                        dispatch(addItemToCart(obj));
+                        return false;
+                    }
+                }
+            }
+        }
+        return orderItem;
+    };
+
+    const repeatOrder = () => {
+        //console.log(items);
+        const notFound = order.items.filter((orderItem) => putItemToCart(orderItem));
+        console.log(notFound);
+
+        // for (let x in items) {
+        //     const category = items[x];
+        //     if (category.length > 0) {
+        //         for (let i = 0; i < category.length; i++) {
+        //             console.log(category[i].article);
+        //             const article = category[i].article
+        //         }
+        //     }
+        // }
+        //console.log(order.items);
+    };
+
     return (
         <HistoryTableBlockStyled>
             <div style={{ fontWeight: "500" }}>Заказ №{order.id}</div>
@@ -53,7 +102,9 @@ function HistoryTableBlock({ order }) {
                 </tbody>
             </Table>
             <Divider>
-                <Button variant="primary">Повторить заказ</Button>
+                <Button variant="primary" onClick={repeatOrder}>
+                    Повторить заказ
+                </Button>
             </Divider>
         </HistoryTableBlockStyled>
     );
