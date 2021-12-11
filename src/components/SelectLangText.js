fc98d9b3bb16283a45f83c4a9c2bf78031c88763
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-11 14:22:19 +0300

    add lang icon

--- src/components/SelectLangText.js
+++ src/components/SelectLangText.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "@emotion/styled";
 import "./lang.css";
+import SelectLangIcon from "./SelectLangIcon";
 
 function SelectLangText({ ...props }, ref) {
     return (
         <div {...props} ref={ref}>
-            Select lang
+            <SelectLangIcon />
+            Lang
         </div>
     );
 }
