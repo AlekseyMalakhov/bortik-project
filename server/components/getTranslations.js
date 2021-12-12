67ecf6d75cfc2024c5eb916bbd488109cb0855b5
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-12 21:31:40 +0300

    generate translations on server

--- server/components/getTranslations.js
+++ server/components/getTranslations.js
@@ -11,9 +11,51 @@ const generateListOfItems = () => {
 const workbook = generateListOfItems();
 const data = workbook.Sheets.Sheet1;
 
+const data2 = {};
+const regex = new RegExp("[A-Z]\\d*");
+for (let x in data) {
+    if (regex.test(x)) {
+        data2[x] = data[x];
+    }
+}
+
+const regex2 = new RegExp("A\\d*");
+let numberOfRows = 0;
+for (let x in data2) {
+    if (regex2.test(x)) {
+        numberOfRows++;
+    }
+}
+
+const ru = {
+    translation: {},
+};
+const zh = {
+    translation: {},
+};
+const en = {
+    translation: {},
+};
+
+for (let i = 2; i < numberOfRows; i++) {
+    ru.translation[data2[`A${i}`].v] = data2[`B${i}`].v;
+    zh.translation[data2[`A${i}`].v] = data2[`C${i}`].v;
+    en.translation[data2[`A${i}`].v] = data2[`D${i}`].v;
+}
+
+const result = {
+    ru,
+    zh,
+    en,
+};
+
+// console.log(ru);
+// console.log(zh);
+// console.log(en);
+
 const getTranslations = (req, res) => {
-    if (data) {
-        res.send(data);
+    if (result) {
+        res.send(result);
     } else {
         res.status(500).send("Server error - please contact administrator");
     }
