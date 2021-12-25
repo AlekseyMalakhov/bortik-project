1c5400ebfd57498853f6e9a7268df02080af1ee9
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-26 00:04:12 +0300

    translate cart table

--- src/components/CartTable.js
+++ src/components/CartTable.js
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import Table from "react-bootstrap/Table";
 import TableRow from "./TableRow";
 import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 
 const CartTableStyled = styled.div({
     width: "100%",
@@ -16,6 +17,7 @@ const EmptyCart = styled.div({
 });
 
 function CartTable({ cart, priceType, sum }) {
+    const { t } = useTranslation();
     const mobileScreen = useSelector((state) => state.manage.mobileScreen);
 
     if (cart.length > 0) {
@@ -24,10 +26,10 @@ function CartTable({ cart, priceType, sum }) {
                 <Table hover>
                     <thead>
                         <tr>
-                            <th style={{ textAlign: "center" }}>Фото</th>
-                            <th>Наименование</th>
-                            <th style={{ textAlign: "center" }}>Кол.</th>
-                            <th style={{ textAlign: "center" }}>Цена</th>
+                            <th style={{ textAlign: "center" }}>{t("Фото")}</th>
+                            <th>{t("Наименование")}</th>
+                            <th style={{ textAlign: "center" }}>{t("Кол.")}</th>
+                            <th style={{ textAlign: "center" }}>{t("Цена")}</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -36,17 +38,19 @@ function CartTable({ cart, priceType, sum }) {
                         ))}
                         <tr style={{ borderBottomStyle: "none" }}>
                             <th colSpan="3" style={{ borderBottomStyle: "none" }}>
-                                Общая сумма
+                                {t("Общая сумма")}
                             </th>
                             <th style={{ textAlign: "center", borderBottomStyle: "none" }}>{sum.toFixed(2)}</th>
                         </tr>
                     </tbody>
                 </Table>
-                <div style={{ fontSize: "16px" }}>Цена: {priceType}</div>
+                <div style={{ fontSize: "16px" }}>
+                    {t("Цена")}: {t(priceType)}
+                </div>
             </CartTableStyled>
         );
     }
-    return <EmptyCart>Корзина пуста</EmptyCart>;
+    return <EmptyCart>{t("Корзина пуста")}</EmptyCart>;
 }
 
 export default CartTable;
--- src/views/Cart.js
+++ src/views/Cart.js
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import { useSelector } from "react-redux";
 import CartTable from "../components/CartTable";
 import CartForm from "../components/CartForm";
+import { useTranslation } from "react-i18next";
 
 const Title = styled.div({
     textAlign: "center",
@@ -22,13 +23,14 @@ const CartStyled = styled.div({
 });
 
 function Cart() {
+    const { t } = useTranslation();
     const cart = useSelector((state) => state.manage.cart);
     const priceType = useSelector((state) => state.manage.priceType);
     const sum = useSelector((state) => state.manage.cartSum);
 
     return (
         <CartStyled>
-            <Title>Оформление заказа</Title>
+            <Title>{t("Оформление заказа")}</Title>
             <CartTable cart={cart} priceType={priceType} sum={sum} />
             <CartForm cart={cart} priceType={priceType} sum={sum} />
         </CartStyled>
