611d36345cf8b798216d79b8b60e86799d24ab18
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-26 19:04:28 +0300

    forgotten password sent modal translate

--- src/components/ForgottenPasswordSentModal.js
+++ src/components/ForgottenPasswordSentModal.js
@@ -2,9 +2,11 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router";
+import { useTranslation } from "react-i18next";
 
 function ForgottenPasswordSentModal({ show, onHide, ...otherProps }) {
     const navigate = useNavigate();
+    const { t } = useTranslation();
 
     const close = () => {
         onHide();
@@ -14,11 +16,11 @@ function ForgottenPasswordSentModal({ show, onHide, ...otherProps }) {
     return (
         <Modal show={show} {...otherProps} size="sm" centered>
             <Modal.Body>
-                <h5 style={{ textAlign: "center", marginBottom: "15px" }}>Пароль восстановлен!</h5>
-                <p style={{ marginBottom: "10px" }}>Пароль выслан Вам на email. Проверьте пожалуйста Ваш почтовый ящик.</p>
+                <h5 style={{ textAlign: "center", marginBottom: "15px" }}>{t("Пароль восстановлен!")}</h5>
+                <p style={{ marginBottom: "10px" }}>{t("Пароль выслан Вам на email. Проверьте пожалуйста Ваш почтовый ящик.")}</p>
             </Modal.Body>
             <Modal.Footer style={{ display: "flex", justifyContent: "center" }}>
-                <Button onClick={close}>На главную</Button>
+                <Button onClick={close}>{t("На главную")}</Button>
             </Modal.Footer>
         </Modal>
     );
