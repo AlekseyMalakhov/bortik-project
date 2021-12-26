473d558ca2b9f1bc40335220e1dc7025f962bbd5
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-26 18:56:08 +0300

    cartsentmodal translate

--- src/components/CartSentModal.js
+++ src/components/CartSentModal.js
@@ -5,10 +5,12 @@ import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
 import { cleanCart } from "../store/manage";
+import { useTranslation } from "react-i18next";
 
 const CartSentModalStyled = styled.div({});
 
 function CartSentModal({ onHide, email, showDone, orderID, ...otherProps }) {
+    const { t } = useTranslation();
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -22,26 +24,26 @@ function CartSentModal({ onHide, email, showDone, orderID, ...otherProps }) {
             {showDone === "OK" ? (
                 <Modal {...otherProps} size="sm" aria-labelledby="contained-modal-title-vcenter" centered>
                     <Modal.Body>
-                        <h5 style={{ textAlign: "center" }}>Заказ отправлен!</h5>
+                        <h5 style={{ textAlign: "center" }}>{t("Заказ отправлен!")}</h5>
                         <p>
-                            Номер Вашего заказа <b>{orderID}</b>. Подтверждение заказа отправлено на Ваш email: <b>{email}</b>
+                            {t("Номер Вашего заказа")} <b>{orderID}</b>. {t("Подтверждение заказа отправлено на Ваш email:")} <b>{email}</b>
                         </p>
-                        <p>В ближайшее время с Вами свяжется наш менеджер для уточнения деталей доставки.</p>
+                        <p>{t("В ближайшее время с Вами свяжется наш менеджер для уточнения деталей доставки.")}</p>
                     </Modal.Body>
                     <Modal.Footer style={{ display: "flex", justifyContent: "center" }}>
-                        <Button onClick={close}>На главную</Button>
+                        <Button onClick={close}>{t("На главную")}</Button>
                     </Modal.Footer>
                 </Modal>
             ) : null}
             {showDone === "Error" ? (
                 <Modal {...otherProps} size="sm" aria-labelledby="contained-modal-title-vcenter" centered>
                     <Modal.Body>
-                        <h5 style={{ textAlign: "center" }}>Произошла ошибка!</h5>
-                        <p>Ваш заказ не отправлен.</p>
-                        <p>Обратитесь в службу поддержки или попробуйте позже.</p>
+                        <h5 style={{ textAlign: "center" }}>{t("Произошла ошибка!")}</h5>
+                        <p>{t("Ваш заказ не отправлен")}.</p>
+                        <p>{t("Обратитесь в службу поддержки или попробуйте позже.")}</p>
                     </Modal.Body>
                     <Modal.Footer style={{ display: "flex", justifyContent: "center" }}>
-                        <Button onClick={() => onHide()}>Закрыть</Button>
+                        <Button onClick={() => onHide()}>{t("Закрыть")}</Button>
                     </Modal.Footer>
                 </Modal>
             ) : null}
