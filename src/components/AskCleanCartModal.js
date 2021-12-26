8aec890c11a3a1af4dc1a26dd3d4576b1dbf564e
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-26 20:19:03 +0300

    clean the cart modal translate

--- src/components/AskCleanCartModal.js
+++ src/components/AskCleanCartModal.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import { useTranslation } from "react-i18next";
 
 function AskCleanCartModal({ show, onHide, onClean, ...otherProps }) {
+    const { t } = useTranslation();
     return (
         <Modal show={show} {...otherProps} size="sm" centered onHide={onHide}>
             <Modal.Body style={{ display: "flex", justifyContent: "center" }}>
-                <p>Очистить корзину?</p>
+                <p>{t("Очистить корзину?")}</p>
             </Modal.Body>
             <Modal.Footer style={{ display: "flex", justifyContent: "space-evenly" }}>
                 <Button onClick={onHide} variant="outline-primary">
-                    Отмена
+                    {t("Отмена")}
                 </Button>
                 <Button onClick={onClean} style={{ width: "81px" }}>
-                    Да
+                    {t("Да")}
                 </Button>
             </Modal.Footer>
         </Modal>
