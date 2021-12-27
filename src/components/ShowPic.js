fad961f7936f04ebd8afad7ae12d236bf5cc6061
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2021-12-27 07:47:24 +0300

    show image

--- src/components/ShowPic.js
+++ src/components/ShowPic.js
@@ -1,17 +1,22 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
+import { useTranslation } from "react-i18next";
 
 function ShowPic({ item, fullscreen, onHide }) {
-    return (
-        <Modal show={item} fullscreen={fullscreen} onHide={onHide}>
-            <Modal.Header closeButton>
-                <h6>{item.title}</h6>
-            </Modal.Header>
-            <Modal.Body>
-                <img src={item.img} alt={""} style={{ maxWidth: "100%", height: "auto" }}></img>
-            </Modal.Body>
-        </Modal>
-    );
+    const { i18n } = useTranslation();
+    if (item) {
+        return (
+            <Modal show={item} fullscreen={fullscreen} onHide={onHide}>
+                <Modal.Header closeButton>
+                    <h6>{item.title[i18n.resolvedLanguage]}</h6>
+                </Modal.Header>
+                <Modal.Body>
+                    <img src={item.img} alt={""} style={{ maxWidth: "100%", height: "auto" }}></img>
+                </Modal.Body>
+            </Modal>
+        );
+    }
+    return null;
 }
 
 export default ShowPic;
