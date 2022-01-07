1a9e2aee362780fc204ac9495d576ef42b1c8786
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-01-07 20:33:08 +0300

    createAccount refactoring

--- server/db/queries.js
+++ server/db/queries.js
@@ -5,32 +5,7 @@ const translations = require("../components/getItemNamesTranslations");
 const pool = require("./pool");
 
 const createAccountAuto = require("./createAccountAuto");
-
-const createAccount = async (req, res) => {
-    const { name, email, password, phone, address } = req.body;
-    const query1 = {
-        text: "SELECT * FROM users WHERE email = $1",
-        values: [email],
-    };
-    try {
-        const response1 = await pool.query(query1);
-        const existingUser = response1.rows[0];
-        if (!existingUser) {
-            const query2 = {
-                text: "INSERT INTO users (name, email, password, phone, address) VALUES($1, $2, $3, $4, $5) RETURNING id",
-                values: [name, email, password, phone, address],
-            };
-            const response2 = await pool.query(query2);
-            res.status(201).send(`User added with ID: ${response2.rows[0].id}`);
-        } else {
-            res.status(409).send("Current email already exists");
-            return null;
-        }
-    } catch (error) {
-        res.status(500).send(error.stack);
-        console.log(error.stack);
-    }
-};
+const createAccount = require("./createAccount");
 
 const editAccount = async (req, res) => {
     const id = req.params.id;
