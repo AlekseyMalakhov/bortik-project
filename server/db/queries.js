79dfcd295defab760289422f160317bdb28efb40
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-01-07 20:27:12 +0300

    pool refactoring

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,42 +1,10 @@
-const Pool = require("pg").Pool;
 require("dotenv").config({ path: "../../project_env/.env" }); //just for dev environment
 const transporter = require("../components/nodeMailerClient");
 const format = require("pg-format");
 const translations = require("../components/getItemNamesTranslations");
+const pool = require("./pool");
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
-
-const createAccountAuto = async (req, res) => {
-    const { name, email, phone, address } = req.body.customer;
-    const query1 = {
-        text: "SELECT * FROM users WHERE email = $1",
-        values: [email],
-    };
-    try {
-        const response1 = await pool.query(query1);
-        const existingUser = response1.rows[0];
-        if (!existingUser) {
-            const query2 = {
-                text: "INSERT INTO users (name, email, password, phone, address) VALUES($1, $2, $3, $4, $5) RETURNING *",
-                values: [name, email, "12345", phone, address],
-            };
-            const response2 = await pool.query(query2);
-            const newUser = response2.rows[0];
-            newUser.new = true;
-            return newUser;
-        } else {
-            return existingUser;
-        }
-    } catch (error) {
-        res.status(500).send(error.stack);
-        console.log(error.stack);
-    }
-};
+const createAccountAuto = require("./createAccountAuto");
 
 const createAccount = async (req, res) => {
     const { name, email, password, phone, address } = req.body;
