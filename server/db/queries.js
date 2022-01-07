b7aef397003ee3e8ca89f591166376f417a5ce99
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-01-07 21:25:12 +0300

    create order refactoring

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,5 +1,5 @@
 require("dotenv").config({ path: "../../project_env/.env" }); //just for dev environment
-const format = require("pg-format");
+
 const translations = require("../components/getItemNamesTranslations");
 const pool = require("./pool");
 
@@ -8,42 +8,7 @@ const createAccount = require("./createAccount");
 const editAccount = require("./editAccount");
 const login = require("./login");
 const forgotPassword = require("./forgotPassword");
-
-const createOrder = async (req, res, userID) => {
-    const { cart, date, priceType, sum } = req.body;
-
-    const arrData = [];
-    for (let i = 0; i < cart.length; i++) {
-        const item = [];
-        item.push(cart[i].article);
-        item.push(cart[i].title);
-        item.push(cart[i].number);
-        item.push(cart[i].price);
-        item.push(cart[i].sum);
-        item.push(userID);
-        item.push(date);
-        arrData.push(item);
-    }
-
-    try {
-        const sql = format("INSERT INTO sold_items (article, title, number, price, sum, customer_id, date) VALUES %L RETURNING id", arrData);
-        const response = await pool.query(sql);
-        if (response.rows.length > 0) {
-            const arrOfItemsId = response.rows.map((item) => item.id);
-            console.log(arrOfItemsId);
-
-            const query2 = {
-                text: "INSERT INTO orders (customer_id, date, price_type, items, sum) VALUES($1, $2, $3, $4, $5) RETURNING id",
-                values: [userID, date, priceType, arrOfItemsId, sum],
-            };
-            const response2 = await pool.query(query2);
-            return response2.rows[0].id;
-        }
-    } catch (error) {
-        res.status(500).send(error.stack);
-        console.log(error.stack);
-    }
-};
+const createOrder = require("./createOrder");
 
 const getHistory = async (req, res) => {
     const { userID } = req.body;
