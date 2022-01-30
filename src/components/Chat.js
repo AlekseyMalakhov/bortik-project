5809652fa312f8bda278980ee1613bfa64544df2
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-01-30 22:19:46 +0300

    waiting from the chat support answer

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
-import liveChat from "../utilities/liveChat"; //https://widget-v4.tidiochat.com/1_82_0/static/js/render.acd930074e27198d61a1.js
+import liveChat from "../utilities/liveChat"; //code.tidio.co/7qifmqgpl3o6vnpck6uawogjsbjrhsot.js
 
 function Chat() {
     useEffect(() => {
         liveChat();
-        window.tidioChatApi.open();
     }, []);
     return <div></div>;
 }
