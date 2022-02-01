d228452c795679bce881ae1b9d336a33bfa79706
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-02-01 23:21:26 +0300

    add prod url

--- src/settings/baseURL.js
+++ src/settings/baseURL.js
@@ -3,7 +3,7 @@ const settings = {
     //dev: "http://192.168.100.242:3010/api",
     dev: "http://192.168.100.7:3010/api",
     demo: "https://bortik-project.herokuapp.com/api",
-    prod: "https://bortik-project.herokuapp.com/api",
+    prod: "https://prod-5a-smartikon.herokuapp.com/api",
 };
 
 const getBaseURL = () => {
