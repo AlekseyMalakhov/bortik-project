3f329df8d87887ae53fef2291f7fc790b163d577
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-02-07 22:25:29 +0300

    install bcrypt

--- src/settings/baseURL.js
+++ src/settings/baseURL.js
@@ -1,20 +1,20 @@
 const settings = {
-    dev: "http://localhost:3010/api",
+    //dev: "http://localhost:3010/api",
     //dev: "http://192.168.100.242:3010/api",
-    //dev: "http://192.168.100.7:3010/api",
+    dev: "http://192.168.100.7:3010/api",
     demo: "https://bortik-project.herokuapp.com/api",
     prod: "https://prod-5a-smartikon.herokuapp.com/api",
 };
 
 const getBaseURL = () => {
-    // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-    //     return settings.dev;
-    // } else {
-    //     return settings.demo;
-    // }
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+        return settings.dev;
+    } else {
+        return settings.demo;
+    }
 
     //for production
-    return settings.prod;
+    //return settings.prod;
 };
 
 const baseURL = getBaseURL();
