269dd33ceb26827965647daabe859be4e6bff92f
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2022-03-18 12:41:13 +0300

    modal for remove order works

--- src/components/AdminDoneModal.js
+++ src/components/AdminDoneModal.js
@@ -1,13 +1,25 @@
 import React from "react";
-import Modal from "react-bootstrap/Modal";
 import { useSelector } from "react-redux";
+import "./additional.css";
+import styled from "@emotion/styled";
+
+const MyModal = styled.div({
+    position: "fixed",
+    top: "20px",
+    left: "50%",
+    transform: "translateX(-50%)",
+    backgroundColor: "white",
+    padding: "20px 20px",
+    borderRadius: "8px",
+    border: "2px solid #43c766",
+    fontWeight: "bold",
+});
 
 function AdminDoneModal() {
     const adminDoneModal = useSelector((state) => state.manage.adminDoneModal);
-    return (
-        <Modal show={adminDoneModal} size="sm" backdropClassName>
-            <Modal.Body style={{ display: "flex", justifyContent: "center" }}>{adminDoneModal}</Modal.Body>
-        </Modal>
-    );
+    if (adminDoneModal) {
+        return <MyModal>{adminDoneModal}</MyModal>;
+    }
+    return null;
 }
 export default AdminDoneModal;
--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -3,7 +3,7 @@ import { setAdminDoneModal } from "../store/manage";
 
 export const showAdminDoneModal = (text) => {
     store.dispatch(setAdminDoneModal(text));
-    // setTimeout(() => {
-    //     store.dispatch(setAdminDoneModal(""));
-    // }, 3000);
+    setTimeout(() => {
+        store.dispatch(setAdminDoneModal(""));
+    }, 3000);
 };
